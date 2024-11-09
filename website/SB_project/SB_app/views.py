f47fa2f54eb44b3f304af6afc899ab77afce41f5
Saihan786 <marshall.s15@hotmail.com>
2024-11-09 17:19:32 +0000

    Add htl view with site model context

--- website/SB_project/SB_app/views.py
+++ website/SB_project/SB_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
-from .models import Site
-from .forms import SiteForm
+from .models import Site, HouseTypes
+from .forms import SiteForm, HouseTypeForm
 
 homepage_template_url = "SB_app/homepage.html"
 settings_template_url = "SB_app/settings.html"
@@ -53,4 +53,35 @@ def settings(request):
 
 
 def htl(request):
+    ht_objects = HouseTypes.objects.all()
+    context = {'ht_objects': ht_objects}
+    
+    if request.method == "POST":
+        form = HouseTypeForm(request.POST)
+        context['form'] = form
+
+        if form.is_valid():
+            name = form.cleaned_data.get('name')
+            print(name)
+
+            try:
+                HouseTypes(name=name).save()
+                context['valid_form'] = True
+            except Exception as e:
+                print(e)
+                unique_name_error = 'HouseTypes with this Name already exists.'
+                if unique_name_error in e.messages:
+                    context['name_violation'] = True
+            
+            return render(request, htl_template_url, context)
+        else:
+            context['invalid_form'] = True
+            return render(request, htl_template_url, context)
+
+    elif request.method == "GET":
+        form = HouseTypeForm()
+        context['form'] = form
+
+        return render(request, htl_template_url, context)
+
     return render(request, template_name=htl_template_url, context=None)
