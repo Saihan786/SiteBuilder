a1464d286e989c7d3fac0755f14632a05a98c482
Saihan786 <marshall.s15@hotmail.com>
2024-12-31 18:17:16 +0000

    Add form for housetype library

--- website/SB_project/SB_app/forms.py
+++ website/SB_project/SB_app/forms.py
@@ -10,3 +10,51 @@ class SiteForm(forms.Form):
         decimal_places=4,
         validators=[MinValueValidator(Decimal('0.0000'))],
     )
+
+class HouseTypeForm(forms.Form):
+    # add validators and testing in general here
+
+    name = forms.CharField(max_length=40)
+    configuration = forms.CharField(max_length=40)
+
+    beds = forms.IntegerField()
+    storeys = forms.IntegerField()
+    sales_sq_ft = forms.DecimalField(
+        max_digits=10,
+        decimal_places=4,
+        validators=[MinValueValidator(Decimal('0.0000'))],
+    )
+    build_sq_ft = forms.DecimalField(
+        max_digits=10,
+        decimal_places=4,
+        validators=[MinValueValidator(Decimal('0.0000'))],
+    )
+    build_cost = forms.DecimalField(
+        max_digits=10,
+        decimal_places=4,
+        validators=[MinValueValidator(Decimal('0.0000'))],
+    )
+    build_cost_divide_sq_ft = forms.DecimalField(
+        max_digits=10,
+        decimal_places=4,
+        validators=[MinValueValidator(Decimal('0.0000'))],
+    )
+    build_weeks = forms.IntegerField()
+    depth = forms.DecimalField(
+        max_digits=10,
+        decimal_places=4,
+        validators=[MinValueValidator(Decimal('0.0000'))],
+    )
+    width = forms.DecimalField(
+        max_digits=10,
+        decimal_places=4,
+        validators=[MinValueValidator(Decimal('0.0000'))],
+    )
+    NDSS = forms.BooleanField()
+    NDSS_Description = forms.CharField(max_length=40)
+    Accessibility = forms.CharField(max_length=40)
+    parking_spaces = forms.DecimalField(
+        max_digits=10,
+        decimal_places=4,
+        validators=[MinValueValidator(Decimal('0.0000'))],
+    )
--- website/SB_project/SB_app/views.py
+++ website/SB_project/SB_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 from .models import Site, HouseTypes
-from .forms import SiteForm
+from .forms import SiteForm, HouseTypeForm
 from .tables import SiteTable, HTLTable
 
 homepage_template_url = "SB_app/homepage.html"
@@ -36,14 +36,14 @@ def homepage(request):
                 if unique_name_error in e.messages:
                     context['name_violation'] = True
             
-            site_table.paginate(page=request.POST.get("page", 1), per_page=4)
+            site_table.paginate(page=request.POST.get("page", 1), per_page=7)
             return render(request, homepage_template_url, context)
         else:  
             context['invalid_form'] = True
             return render(request, homepage_template_url, context)
 
     elif request.method == "GET":
-        site_table.paginate(page=request.GET.get("page", 1), per_page=4)
+        site_table.paginate(page=request.GET.get("page", 1), per_page=7)
         form = SiteForm()
         context['form'] = form
 
@@ -69,15 +69,63 @@ def htl(request):
 
     context = {'housetype_objects': housetype_objects, 'housetype_table': housetype_table}
 
-    # site_table.paginate(page=request.POST.get("page", 1), per_page=4)
-    # site_table.paginate(page=request.GET.get("page", 1), per_page=4)
-
-
-
     if request.method == "POST":
-        pass
+        form = HouseTypeForm(request.POST)
+        context['form'] = form
+
+        if form.is_valid():
+            name = form.cleaned_data.get('name')
+            configuration = form.cleaned_data.get('configuration')
+            beds = form.cleaned_data.get('beds')
+            storeys = form.cleaned_data.get('storeys')
+            sales_sq_ft = form.cleaned_data.get('sales_sq_ft')
+            build_sq_ft = form.cleaned_data.get('build_sq_ft')
+            build_cost = form.cleaned_data.get('build_cost')
+            build_cost_divide_sq_ft = form.cleaned_data.get('build_cost_divide_sq_ft')
+            build_weeks = form.cleaned_data.get('build_weeks')
+            depth = form.cleaned_data.get('depth')
+            width = form.cleaned_data.get('width')
+            NDSS = form.cleaned_data.get('NDSS')
+            NDSS_Description = form.cleaned_data.get('NDSS_Description')
+            Accessibility = form.cleaned_data.get('Accessibility')
+            parking_spaces = form.cleaned_data.get('parking_spaces')
+
+            try:
+                HouseTypes(
+                    name=name,
+                    configuration=configuration,
+                    beds=beds,
+                    storeys=storeys,
+                    sales_sq_ft=sales_sq_ft,
+                    build_sq_ft=build_sq_ft,
+                    build_cost=build_cost,
+                    build_cost_divide_sq_ft=build_cost_divide_sq_ft,
+                    build_weeks=build_weeks,
+                    depth=depth,
+                    width=width,
+                    NDSS=NDSS,
+                    NDSS_Description=NDSS_Description,
+                    Accessibility=Accessibility,
+                    parking_spaces=parking_spaces,
+                ).save()
+                context['valid_form'] = True
+            except Exception as e:
+                unique_name_error = 'House type with this Name already exists.'
+                if unique_name_error in e.messages:
+                    context['name_violation'] = True
+            
+            housetype_table.paginate(page=request.POST.get("page", 1), per_page=7)
+            return render(request, htl_template_url, context)
+        else:  
+            context['invalid_form'] = True
+            return render(request, htl_template_url, context)
+
 
     elif request.method == "GET":
+        housetype_table.paginate(page=request.GET.get("page", 1), per_page=7)
+        form = HouseTypeForm()
+        context['form'] = form
+
         return render(request, htl_template_url, context)
 
     return render(request, template_name=htl_template_url, context=context)
